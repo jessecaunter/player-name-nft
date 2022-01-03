720f66c2af3841a3f15bf08d7feb593dc067d2f8
Jesse Caunter <js.caunter@gmail.com>
2022-01-03 16:18:32 +1300

    Set URI as a link to JSON file

--- contracts/PlayerNameNFT.sol
+++ contracts/PlayerNameNFT.sol
@@ -28,7 +28,8 @@ contract PlayerNameNFT is ERC721URIStorage {
         _safeMint(msg.sender, newItemId);
 
         // Set the NFTs data.
-        _setTokenURI(newItemId, "blah");
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/3KNM");
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
         // Increment the counter for when the next NFT is minted.
         _tokenIds.increment();

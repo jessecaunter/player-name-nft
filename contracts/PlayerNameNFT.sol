6ea16e0e635b8d9a83f3cbb9f39c2bb4bb2d7d0d
Jesse Caunter <js.caunter@gmail.com>
2022-01-03 14:48:59 +1300

    Write starter contract

---
+++ contracts/PlayerNameNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract PlayerNameNFT {
+    constructor () {
+        console.log("Are you ready for tip-off?");
+    }
+}
